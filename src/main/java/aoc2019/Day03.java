e03012f6556757f26a4e83bfb60ba552afeafbf0
Pieter Drijkoningen <pieter_drijkoningen@hotmail.com>
2019-12-03 23:47:40 +0100

    2019-03 -> more functional

--- src/main/java/aoc2019/Day03.java
+++ src/main/java/aoc2019/Day03.java
@@ -2,19 +2,19 @@ package aoc2019;
 
 import util.Day;
 import util.Direction;
+import util.Pair;
 import util.Point;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.stream;
 
 @SuppressWarnings("OptionalGetWithoutIsPresent")
 public class Day03 implements Day<List<String>> {
-
-    private final Point ORIGIN = new Point(0, 0);
 
     public static void main(String[] args) throws Exception {
         List<String> input = Files.readAllLines(Paths.get("src/main/resources/2019/day03.txt"));
@@ -23,12 +23,12 @@ public class Day03 implements Day<List<String>> {
 
     @Override
     public String part1(List<String> input) {
-        Set<Point> wire1 = new HashSet<>(plotWire(input.get(0)));
-        Set<Point> wire2 = new HashSet<>(plotWire(input.get(1)));
+        Point origin = new Point(0, 0);
+        Wire wire1 = plotWire(input.get(0));
+        Wire wire2 = plotWire(input.get(1));
         int result = wire1.stream()
-                .filter(point -> !point.equals(ORIGIN))
                 .filter(wire2::contains)
-                .mapToInt(point -> point.distanceTo(ORIGIN))
+                .mapToInt(point -> point.distanceTo(origin))
                 .min()
                 .getAsInt();
         return String.valueOf(result);
@@ -36,31 +36,58 @@ public class Day03 implements Day<List<String>> {
 
     @Override
     public String part2(List<String> input) {
-        List<Point> wire1 = plotWire(input.get(0));
-        List<Point> wire2 = plotWire(input.get(1));
-        Set<Point> wire2Points = new HashSet<>(wire2);
+        Wire wire1 = plotWire(input.get(0));
+        Wire wire2 = plotWire(input.get(1));
         int result = wire1.stream()
-                .filter(point -> !point.equals(ORIGIN))
-                .filter(wire2Points::contains)
+                .filter(wire2::contains)
                 .mapToInt(point -> wire1.indexOf(point) + wire2.indexOf(point))
                 .min()
                 .getAsInt();
         return String.valueOf(result);
     }
 
-    private List<Point> plotWire(String input) {
-        List<Point> wire = new ArrayList<>();
-        Point end = ORIGIN;
-        wire.add(end);
-        for (String step : input.split(",")) {
-            Direction direction = Direction.of(step.charAt(0));
-            int n = Integer.parseInt(step.substring(1));
-            for (int i = 0; i < n; i++) {
-                Point next = end.apply(direction);
-                wire.add(next);
-                end = next;
-            }
+    private Wire plotWire(String input) {
+        Wire wire = new Wire();
+        stream(input.split(","))
+                .map(step -> new Pair<>(Direction.of(step.charAt(0)), Integer.parseInt(step.substring(1))))
+                .forEach(step -> IntStream.range(0, step.right).forEach(i -> wire.add(wire.end().apply(step.left))));
+        return wire.finished();
+    }
+
+    private static class Wire {
+
+        private List<Point> points;
+        private Set<Point> pointSet;
+
+        private Wire() {
+            this.points = new ArrayList<>();
+            points.add(new Point(0, 0));
         }
-        return wire;
+
+        private Point end() {
+            return points.get(points.size() - 1);
+        }
+
+        private void add(Point point) {
+            points.add(point);
+        }
+
+        public Wire finished() {
+            this.points = Collections.unmodifiableList(points);
+            this.pointSet = new HashSet<>(points);
+            return this;
+        }
+
+        private Stream<Point> stream() {
+            return points.stream().skip(1);
+        }
+
+        public boolean contains(Point point) {
+            return pointSet.contains(point);
+        }
+
+        public int indexOf(Point point) {
+            return points.indexOf(point);
+        }
     }
 }
