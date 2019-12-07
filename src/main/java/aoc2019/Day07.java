60a2a82010d8e23f2d26857a0cd6cd19c6d10db0
Pieter Drijkoningen <pieter_drijkoningen@hotmail.com>
2019-12-07 14:49:16 +0100

    2019-07

---
+++ src/main/java/aoc2019/Day07.java
@@ -0,0 +1,80 @@
+package aoc2019;
+
+import aoc2019.intcode.Computer;
+import util.Day;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+
+@SuppressWarnings("OptionalGetWithoutIsPresent")
+public class Day07 implements Day<int[]> {
+
+    public static void main(String[] args) {
+        int[] program = {3, 8, 1001, 8, 10, 8, 105, 1, 0, 0, 21, 34, 55, 68, 85, 106, 187, 268, 349, 430, 99999, 3, 9, 1001, 9, 5, 9, 1002, 9, 5, 9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 1001, 9, 2, 9, 1002, 9, 5, 9, 1001, 9, 2, 9, 4, 9, 99, 3, 9, 101, 3, 9, 9, 102, 3, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 5, 9, 101, 3, 9, 9, 102, 5, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 4, 9, 1001, 9, 2, 9, 102, 3, 9, 9, 101, 3, 9, 9, 4, 9, 99, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 99, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 99};
+        new Day07().run(program);
+    }
+
+    @Override
+    public String part1(int[] program) {
+        try {
+            int maxOutput = Files.readAllLines(Paths.get("src/main/resources/2019/day07_permutations.txt")).stream()
+                    .map(line -> stream(line.split(",")).mapToInt(Integer::parseInt).toArray())
+                    .mapToInt(phaseSetting -> testPhaseSetting(program, phaseSetting))
+                    .max()
+                    .getAsInt();
+            return String.valueOf(maxOutput);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int testPhaseSetting(int[] program, int[] phaseSetting) {
+        int ampInput = 0;
+        for (int setting : phaseSetting) {
+            ampInput = Computer.run(program, setting, ampInput).get(0);
+        }
+        return ampInput;
+    }
+
+    @Override
+    public String part2(int[] program) {
+        try {
+            int maxOutput = Files.readAllLines(Paths.get("src/main/resources/2019/day07_permutations2.txt")).stream()
+                    .map(line -> stream(line.split(",")).mapToInt(Integer::parseInt).toArray())
+                    .mapToInt(phaseSetting -> testFeedbackLoop(program, phaseSetting))
+                    .max()
+                    .getAsInt();
+            return String.valueOf(maxOutput);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Integer testFeedbackLoop(int[] program, int[] phaseSetting) {
+        try {
+            ArrayBlockingQueue<Integer> aInput = new ArrayBlockingQueue<>(10, true, asList(phaseSetting[0], 0));
+            ArrayBlockingQueue<Integer> bInput = new ArrayBlockingQueue<>(10, true, asList(phaseSetting[1]));
+            ArrayBlockingQueue<Integer> cInput = new ArrayBlockingQueue<>(10, true, asList(phaseSetting[2]));
+            ArrayBlockingQueue<Integer> dInput = new ArrayBlockingQueue<>(10, true, asList(phaseSetting[3]));
+            ArrayBlockingQueue<Integer> eInput = new ArrayBlockingQueue<>(10, true, asList(phaseSetting[4]));
+            Computer a = new Computer(program, aInput, bInput);
+            Computer b = new Computer(program, bInput, cInput);
+            Computer c = new Computer(program, cInput, dInput);
+            Computer d = new Computer(program, dInput, eInput);
+            Computer e = new Computer(program, eInput, aInput);
+            asList(a, b, c, d, e).forEach(Thread::start);
+            e.join();
+            return aInput.poll();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/aoc2019/intcode/Computer.java
+++ src/main/java/aoc2019/intcode/Computer.java
@@ -2,29 +2,68 @@ package aoc2019.intcode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
-public class Computer {
+public class Computer extends Thread {
 
     private int instructionPointer;
     private int inputPointer;
     private int[] intCode;
     private List<Integer> outputs;
+    private BlockingQueue<Integer> inputQueue;
+    private BlockingQueue<Integer> outputQueue;
 
     public Computer(int[] intCode) {
         this.instructionPointer = 0;
         this.inputPointer = 0;
-        this.intCode = intCode;
+        this.intCode = new int[intCode.length];
+        System.arraycopy(intCode, 0, this.intCode, 0, intCode.length);
         this.outputs = new ArrayList<>();
     }
 
-    public static List<Integer> run(int[] intCode, int... input) {
-        int[] intCodeCopy = new int[intCode.length];
-        System.arraycopy(intCode, 0, intCodeCopy, 0, intCode.length);
-        return new Computer(intCodeCopy).run(input);
+    public Computer(int[] intCode, BlockingQueue<Integer> inputQueue, BlockingQueue<Integer> outputQueue) {
+        this.instructionPointer = 0;
+        this.inputPointer = 0;
+        this.intCode = new int[intCode.length];
+        System.arraycopy(intCode, 0, this.intCode, 0, intCode.length);
+        this.outputs = new ArrayList<>();
+        this.inputQueue = inputQueue;
+        this.outputQueue = outputQueue;
     }
 
-    @SuppressWarnings("DuplicatedCode")
+    public static List<Integer> run(int[] intCode, int... input) {
+        return new Computer(intCode).run(input);
+    }
+
     private List<Integer> run(int... input) {
+        run(() -> input[inputPointer++], outputs::add);
+        return outputs;
+    }
+
+    @Override
+    public void run() {
+        run(this::takeInput, this::putOutput);
+    }
+
+    private void putOutput(Integer output) {
+        try {
+            outputQueue.put(output);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int takeInput() {
+        try {
+            return inputQueue.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void run(Supplier<Integer> inputSupplier, Consumer<Integer> outputConsumer) {
         while (true) {
             int command = intCode[instructionPointer];
             int opcode = command % 100;
@@ -42,9 +81,9 @@ public class Computer {
                 intCode[outputAddress] = a * b;
             } else if (opcode == 3) {
                 int writeAddress = intCode[++instructionPointer];
-                intCode[writeAddress] = input[inputPointer++];
+                intCode[writeAddress] = inputSupplier.get();
             } else if (opcode == 4) {
-                outputs.add(getValue(parameterMode1));
+                outputConsumer.accept(getValue(parameterMode1));
             } else if (opcode == 5) {
                 int param1 = getValue(parameterMode1);
                 int param2 = getValue(parameterMode2);
@@ -70,7 +109,7 @@ public class Computer {
                 int outputAddress = intCode[++instructionPointer];
                 intCode[outputAddress] = a == b ? 1 : 0;
             } else if (opcode == 99) {
-                return outputs;
+                return;
             } else {
                 throw new RuntimeException("Invalid opcode encountered: " + opcode + " at address " + instructionPointer);
             }
