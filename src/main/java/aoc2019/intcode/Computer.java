baf8afe09ee86e97999020bc9a88ace6a2ae6097
Pieter Drijkoningen <pieter_drijkoningen@hotmail.com>
2019-12-09 21:01:33 +0100

    2019-09

--- src/main/java/aoc2019/intcode/Computer.java
+++ src/main/java/aoc2019/intcode/Computer.java
@@ -14,12 +14,14 @@ public class Computer {
 
     private long instructionPointer;
     private long inputPointer;
+    private long relativeBase;
     private Map<Long, Long> memory;
     private List<Long> outputs;
 
     public Computer(long[] intCode) {
         this.instructionPointer = 0;
         this.inputPointer = 0;
+        this.relativeBase = 0;
         this.outputs = new ArrayList<>();
         this.memory = new HashMap<>();
         IntStream.range(0, intCode.length).forEach(i -> memory.put((long) i, intCode[i]));
@@ -36,50 +38,52 @@ public class Computer {
 
     protected void run(Supplier<Long> inputSupplier, Consumer<Long> outputConsumer) {
         while (true) {
-            long command = memory.get(instructionPointer);
+            long command = memory.getOrDefault(instructionPointer, 0L);
             long opcode = command % 100;
             long parameterMode1 = command % 1000 / 100;
             long parameterMode2 = command % 10000 / 1000;
             long parameterMode3 = command / 10000;
             if (opcode == 1) {
-                long a = getValue(parameterMode1);
-                long b = getValue(parameterMode2);
+                long a = getParameter(parameterMode1);
+                long b = getParameter(parameterMode2);
                 long writeAddress = getWriteAddress(parameterMode3);
                 memory.put(writeAddress, a + b);
             } else if (opcode == 2) {
-                long a = getValue(parameterMode1);
-                long b = getValue(parameterMode2);
+                long a = getParameter(parameterMode1);
+                long b = getParameter(parameterMode2);
                 long writeAddress = getWriteAddress(parameterMode3);
                 memory.put(writeAddress, a * b);
             } else if (opcode == 3) {
                 long writeAddress = getWriteAddress(parameterMode1);
                 memory.put(writeAddress, inputSupplier.get());
             } else if (opcode == 4) {
-                outputConsumer.accept(getValue(parameterMode1));
+                outputConsumer.accept(getParameter(parameterMode1));
             } else if (opcode == 5) {
-                long param1 = getValue(parameterMode1);
-                long param2 = getValue(parameterMode2);
+                long param1 = getParameter(parameterMode1);
+                long param2 = getParameter(parameterMode2);
                 if (param1 != 0) {
                     instructionPointer = param2;
                     continue;
                 }
             } else if (opcode == 6) {
-                long param1 = getValue(parameterMode1);
-                long param2 = getValue(parameterMode2);
+                long param1 = getParameter(parameterMode1);
+                long param2 = getParameter(parameterMode2);
                 if (param1 == 0) {
                     instructionPointer = param2;
                     continue;
                 }
             } else if (opcode == 7) {
-                long a = getValue(parameterMode1);
-                long b = getValue(parameterMode2);
+                long a = getParameter(parameterMode1);
+                long b = getParameter(parameterMode2);
                 long writeAddress = getWriteAddress(parameterMode3);
                 memory.put(writeAddress, a < b ? 1L : 0);
             } else if (opcode == 8) {
-                long a = getValue(parameterMode1);
-                long b = getValue(parameterMode2);
+                long a = getParameter(parameterMode1);
+                long b = getParameter(parameterMode2);
                 long writeAddress = getWriteAddress(parameterMode3);
                 memory.put(writeAddress, a == b ? 1L : 0);
+            } else if (opcode == 9) {
+                relativeBase += getParameter(parameterMode1);
             } else if (opcode == 99) {
                 return;
             } else {
@@ -89,12 +93,22 @@ public class Computer {
         }
     }
 
-    private long getValue(long parameterMode) {
-        long memoryValue = memory.get(++instructionPointer);
-        return parameterMode == 0 ? memory.get(memoryValue) : memoryValue;
+    private long getParameter(long parameterMode) {
+        long memoryValue = memory.getOrDefault(++instructionPointer, 0L);
+        return switch (toIntExact(parameterMode)) {
+            case 0 -> memory.getOrDefault(memoryValue, 0L);
+            case 1 -> memoryValue;
+            case 2 -> memory.getOrDefault(memoryValue + relativeBase, 0L);
+            default -> throw new RuntimeException("Invalid parameterMode: " + parameterMode);
+        };
     }
 
     private long getWriteAddress(long parameterMode) {
-        return memory.get(++instructionPointer);
+        long memoryValue = memory.getOrDefault(++instructionPointer, 0L);
+        return switch (toIntExact(parameterMode)) {
+            case 0 -> memoryValue;
+            case 2 -> memoryValue + relativeBase;
+            default -> throw new RuntimeException("Invalid parameterMode: " + parameterMode);
+        };
     }
 }
